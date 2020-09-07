c1270f91f6dd4427500b9fd718f09fd27910ae05
David Rakosi <david.rakosi99@gmail.com>
2020-09-07 18:24:35 +0200

    Updates

---
+++ precache-manifest.939cfe224d004d452ac5ea5abb5de86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fffb0d34ed2285b79c21be7a835393d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0f90edffd770988fc5bd",
+    "url": "/react-portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/react-portfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-portfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f90edffd770988fc5bd",
+    "url": "/react-portfolio/static/js/main.996b6103.chunk.js"
+  },
+  {
+    "revision": "ea608e5f497480426bb5",
+    "url": "/react-portfolio/static/js/runtime-main.7540febe.js"
+  }
+]);
